ab696264c64538609717d2ff2c95ae37bfcb6ccb
Arne Dußin <arne.dussin@gmail.com>
2020-12-04 18:24:40 +0100

    Add solution to 04.b

--- src/04/main.rs
+++ src/04/main.rs
@@ -2,7 +2,19 @@ use std::collections::HashMap;
 use std::fs;
 use std::str::FromStr;
 
-pub const PASSPORT_REQUIRED_FIELDS: [&str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+pub const BIRTH_YEAR: &str = "byr";
+pub const ISSUE_YEAR: &str = "iyr";
+pub const EXP_YEAR: &str = "eyr";
+pub const HEIGHT: &str = "hgt";
+pub const HAIRCOLOUR: &str = "hcl";
+pub const EYECOLOUR: &str = "ecl";
+pub const PASS_ID: &str = "pid";
+
+pub const COUNTRY_ID: &str = "cid";
+
+pub const PASSPORT_REQUIRED_FIELDS: [&str; 7] = [
+    BIRTH_YEAR, ISSUE_YEAR, EXP_YEAR, HEIGHT, HAIRCOLOUR, EYECOLOUR, PASS_ID,
+];
 
 #[derive(Debug, thiserror::Error)]
 enum PassportParseErr {
@@ -18,9 +30,107 @@ struct Passport {
     fields: HashMap<String, String>,
 }
 
+fn check_year(s: &str, min: u32, max: u32) -> bool {
+    match s.parse::<u32>() {
+        Ok(year) => year >= min && year <= max,
+        Err(_) => false,
+    }
+}
+
 impl Passport {
-    pub fn get(&self, field: &str) -> Option<&String> {
-        self.fields.get(field)
+    /// Checks if all required values are ok according to the rules in b)
+    pub fn values_ok(&self) -> bool {
+        let birthyear = self.fields.get(BIRTH_YEAR).unwrap();
+        if !check_year(birthyear, 1920, 2002) {
+            println!("Birthyear invalid: {}", birthyear);
+            return false;
+        }
+        let issue_year = self.fields.get(ISSUE_YEAR).unwrap();
+        if !check_year(issue_year, 2010, 2020) {
+            println!("Issue year invalid: {}", birthyear);
+            return false;
+        }
+        let exp_year = self.fields.get(EXP_YEAR).unwrap();
+        if !check_year(exp_year, 2020, 2030) {
+            println!("Expiration year invalid: {}", birthyear);
+            return false;
+        }
+
+        // Check height
+        let mut height = self.fields.get(HEIGHT).unwrap().clone();
+        let cm = height.ends_with("cm");
+        if !cm && !height.ends_with("in") {
+            println!("Height {} does not end in cm or in", height);
+            return false;
+        }
+        height.pop();
+        height.pop();
+        let height = match height.parse::<u8>() {
+            Ok(height) => height,
+            Err(_) => {
+                println!("Could not parse int from height {}", height);
+                return false;
+            }
+        };
+        if cm && height >= 150 && height <= 193 {
+        } else if !cm && height >= 59 && height <= 76 {
+        } else {
+            if cm {
+                println!("{}cm not a valid height.", height);
+            } else {
+                println!("{}in not a valid height.", height);
+            }
+            return false;
+        }
+
+        // Check haircolour
+        let haircolour = self.fields.get(HAIRCOLOUR).unwrap();
+        if haircolour.len() != 7 || haircolour.chars().nth(0).unwrap() != '#' {
+            println!("Not a valid haircolour: {}", haircolour);
+            return false;
+        }
+        for c in haircolour.chars().skip(1) {
+            match c {
+                '0'..='9' | 'a'..='f' => {}
+                o => {
+                    println!("{}-character invalid in haircolour.", o);
+                    return false;
+                }
+            }
+        }
+
+        // Check eyecolour
+        match self.fields.get(EYECOLOUR).unwrap().as_str() {
+            "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => {}
+            o => {
+                println!("{} not a valid eye colour.", o);
+                return false;
+            }
+        }
+
+        // Check passport id
+        let passport_id = self.fields.get(PASS_ID).unwrap();
+        if passport_id.len() != 9 {
+            println!(
+                "Invalid passport id length: {} instead of 9",
+                passport_id.len()
+            );
+            return false;
+        }
+        for c in passport_id.chars() {
+            match c {
+                '0'..='9' => {}
+                o => {
+                    println!(
+                        "Passport id must consist only of numbers, which {} is not.",
+                        o
+                    );
+                    return false;
+                }
+            }
+        }
+
+        true
     }
 }
 
@@ -70,4 +180,13 @@ fn main() {
     }
 
     println!("Number of valid passports for a) {}", passports.len());
+
+    let mut num_valid = 0;
+    for passport in passports {
+        if passport.values_ok() {
+            num_valid += 1;
+        }
+    }
+
+    println!("Number of valid passports for b) {}", num_valid);
 }
