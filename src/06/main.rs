f3b2957e1f7ccd4af1b7a01428cf628df8d51d38
Arne Dußin <arne.dussin@gmail.com>
2020-12-06 12:24:16 +0100

    Add solution to 06.b

--- src/06/main.rs
+++ src/06/main.rs
@@ -3,36 +3,53 @@ use std::fs;
 // Only lowercase, standard latin alphabet are allowed.
 const NUM_CHARS: usize = 26;
 
-fn find_any_yes_answer(s: &str) -> [bool; NUM_CHARS] {
-    let mut answers = [false; NUM_CHARS];
+// Count the number of yes answers of a group. Returns also the number of people in that group for
+// reference.
+fn count_yes_answers(s: &str) -> ([usize; NUM_CHARS], usize) {
+    // Trimming is necessary for the last element to be counted properly. Otherwise it thinks there
+    // is one more person in the group.
+    let s = s.trim_end();
 
+    let mut answers = [0; NUM_CHARS];
+    let mut num_people = 1;
     for c in s.chars() {
         match c {
-            'a'..='z' => answers[c as usize - 'a' as usize] = true,
-            '\n' => {}
+            'a'..='z' => answers[c as usize - 'a' as usize] += 1,
+            '\n' => num_people += 1,
             other => panic!("Unexpected character: '{}'", other),
         }
     }
 
-    answers
+    (answers, num_people)
 }
 
 fn main() {
     let input = fs::read_to_string("input/06").expect("Could not read input file");
     let group_inputs = input.split("\n\n");
 
-    let mut num_yes_answers = 0;
+    let mut num_any_yes = 0;
+    let mut num_all_yes = 0;
     for group in group_inputs {
-        let yes_answers = find_any_yes_answer(&group);
+        let (yes_answers, num_people) = count_yes_answers(&group);
+
         for a in yes_answers.iter() {
-            if *a {
-                num_yes_answers += 1;
+            assert!(*a <= num_people);
+            if *a >= 1 {
+                num_any_yes += 1;
+            }
+            if *a == num_people {
+                num_all_yes += 1;
             }
         }
     }
 
     println!(
         "Total number of questions anyone answered yes for a) {}",
-        num_yes_answers
+        num_any_yes
+    );
+
+    println!(
+        "Total number of questions all answered yes for b) {}",
+        num_all_yes
     );
 }
